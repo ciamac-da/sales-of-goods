707e6548dc3480c231d273043e6787e631ce741d
ciamac <hgzguzg>
2019-09-12 11:47:58 +0200

    Modified(1) by Cia

--- register.js
+++ register.js
@@ -1,1 +1,26 @@
-function 
+var summe = 0
+var geldbezahlt
+function GesamtSumme (summebetrag){
+    summe += summebetrag
+
+    return summe
+}
+
+function Rückgeld (geldbezahlt){
+    var rückbetrag = geldbezahlt - summe
+    return rückbetrag
+}
+
+console.log(x);
+
+
+
+/*
+var summe = 20
+
+summe+= 100
+
+summ+= 200
+
+summe=320
+*/ 
